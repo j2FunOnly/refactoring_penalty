9fabc348e818a4a4f048081c330452eff4d26937
j2FunOnly <j2funonly@ya.ru>
2016-08-26 12:38:20 +0300

    Refactor specs

--- spec/penalty_spec.rb
+++ spec/penalty_spec.rb
@@ -1,37 +1,35 @@
 require 'penalty'
 require_relative 'support/custom_policy'
 
-module Penalty
-  RSpec.describe Calculator do
-    include Table
+RSpec.describe Penalty::Calculator do
+  include Penalty::Table
 
-    it 'return penalty with default policy' do
-      kpi = 95
-      client = '2'
+  let(:kpi) { 95 }
+  let(:client) { '2' }
 
+  describe 'return penalty' do
+    it 'with default policy' do
       a, b = subject.get_penalty(kpi, client)
       expect([a, b]).to eq penalty_table[0][0]
     end
 
-    it 'return penalty with custom policy' do
-      kpi = 95
-      client = '2'
+    it 'with custom policy' do
       calculator = described_class.new
       calculator.policy = CustomPolicy
 
       a, b = calculator.get_penalty(kpi, client)
       expect([a, b]).to eq penalty_table[0][1]
     end
+  end
 
+  describe 'raise error' do
     it 'raise error when no kpi in penalty table' do
       kpi = 800
-      client = '2'
       expect{ subject.get_penalty(kpi, client) }
         .to raise_error ArgumentError, 'No penalty for kpi = 800'
     end
 
     it 'raise error when wrong policy' do
-      kpi = 250
       client = 'wrong client'
       expect{ subject.get_penalty(kpi, client) }
         .to raise_error ArgumentError, "No Policy for client = #{client}"
